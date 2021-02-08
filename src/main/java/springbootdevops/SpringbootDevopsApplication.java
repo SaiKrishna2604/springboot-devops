b7b56eb3b8389bee808bb882e2591520a7915c12
Saikrishna26 <kvm.saikrishna@gmail.com>
2021-02-08 17:50:03 +0530

    added logging to the application

--- src/main/java/springbootdevops/SpringbootDevopsApplication.java
+++ src/main/java/springbootdevops/SpringbootDevopsApplication.java
@@ -1,5 +1,7 @@
 package springbootdevops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringbootDevopsApplication {
+	static Logger logger = LoggerFactory.getLogger(SpringbootDevopsApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootDevopsApplication.class, args);
 	}
 	
 	@GetMapping("/")
 	public String testMethod() {
-		return "welcome to Sai Krishna tutorials!!";
+		logger.info("welcome message");
+		return "welcome to Sai Krishna V2 tutorials!!";
 	}
 
 }
